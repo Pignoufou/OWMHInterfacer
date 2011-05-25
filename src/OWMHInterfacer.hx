f7b3a794735a3bb96ec70e412f10a29fbfb13ae0
Benjamin Dasnois <benjamin@Calcifer.local>
2011-05-25 23:56:44 +0200

    Initial commit. Project creation.

---
+++ src/OWMHInterfacer.hx
@@ -0,0 +1,14 @@
+class OWMHInterfacer
+{
+
+	public function new()
+	{
+		
+	}
+
+	public static function main(): Void
+	{
+		var m: OWMHInterfacer = new OWMHInterfacer();
+		trace('Hello World');
+	}
+}
